744ff190ab3f7499f249409c378105bf3f4888b4
Malcolm Maclean <2288114M@student.gla.ac.uk>
2018-02-08 17:06:50 +0000

    sixth commit chpt 6 and excercizes completed

--- rango/views.py
+++ rango/views.py
@@ -8,8 +8,9 @@ from rango.models import Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
-
+    page_list = Page.objects.order_by('-views')[:5]
+    
+    context_dict = {'categories': category_list, 'pages': page_list}
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
