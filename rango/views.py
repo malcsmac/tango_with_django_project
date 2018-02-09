cd70e367e9db498101ea7963142a301ea038c246
Malcolm Maclean <2288114M@student.gla.ac.uk>
2018-02-09 13:13:41 +0000

    chapter 8

--- rango/views.py
+++ rango/views.py
@@ -18,8 +18,11 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    context_dict = {}
-    return render(request, 'rango/about.html', context=context_dict)
+    # prints out whether the method is a GET or a POST
+    print(request.method)
+    # prints out the user name, if no one is logged in it prints `AnonymousUser`
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 
 
 
