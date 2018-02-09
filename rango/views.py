f4ae82c07f10596fae8d5cf53760271504f6fe51
Malcolm Maclean <2288114M@student.gla.ac.uk>
2018-02-09 17:16:07 +0000

    chpt 9 with excercize

--- rango/views.py
+++ rango/views.py
@@ -20,14 +20,25 @@ from django.contrib.auth.decorators import login_required
 
 from django.contrib.auth import logout
 
+from datetime import datetime
+
+# Create your views here.
+
+
+
 def index(request):
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-    
     context_dict = {'categories': category_list, 'pages': page_list}
-    return render(request, 'rango/index.html', context_dict)
-
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
     # prints out whether the method is a GET or a POST
     print(request.method)
     # prints out the user name, if no one is logged in it prints `AnonymousUser`
@@ -65,7 +76,7 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
     # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context_dict)
-
+@login_required
 def add_category(request):
     form = CategoryForm()
     # A HTTP POST?
@@ -87,7 +98,7 @@ def add_category(request):
 # Will handle the bad form, new form, or no form supplied cases.
 # Render the form with error messages (if any).
     return render(request, 'rango/add_category.html', {'form': form})
-
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -214,4 +225,26 @@ def user_logout(request):
     # Take the user back to the homepage.
     return HttpResponseRedirect(reverse('index'))
 
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+# Updated the function definition
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).seconds > 0:
+            visits = visits + 1
+            #update the last visit cookie now that we have updated the count
+            request.session['last_visit'] = str(datetime.now())
+    else:
+        visits = 1
+        # set the last visit cookie 
+        request.session['last_visit'] = last_visit_cookie
+    # update/set the visits cookie
+    request.session['visits'] = visits
 
